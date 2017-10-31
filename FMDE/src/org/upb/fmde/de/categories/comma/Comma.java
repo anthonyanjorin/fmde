441697cbb47d22580637b7ed2d21b21c3191240e
nilsweid <nilsweid@mail.upb.de>
2017-10-31 18:27:36 +0100

    Initial creation of Comma and Slice classes.

---
+++ FMDE/src/org/upb/fmde/de/categories/comma/Comma.java
@@ -0,0 +1,44 @@
+package org.upb.fmde.de.categories.comma;
+
+import org.upb.fmde.de.categories.Category;
+import org.upb.fmde.de.categories.LabelledArrow;
+
+public class Comma<L, R extends LabelledArrow<L>> implements Category<L, R> {
+
+	@Override
+	public R compose(R f, R g) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public R id(L o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public L source(R f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public L target(R f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String showOb(L o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String showArr(R f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
