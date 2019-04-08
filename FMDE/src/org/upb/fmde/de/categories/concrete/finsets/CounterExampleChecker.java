86e3ae62afe24337e328702d9cae1972ecf1f1fd
Anthony Anjorin <tonyanjorin@googlemail.com>
2019-04-08 16:14:35 +0200

    Make better use of generics

--- FMDE/src/org/upb/fmde/de/categories/concrete/finsets/CounterExampleChecker.java
+++ FMDE/src/org/upb/fmde/de/categories/concrete/finsets/CounterExampleChecker.java
@@ -1,27 +1,34 @@
 package org.upb.fmde.de.categories.concrete.finsets;
 
+import org.upb.fmde.de.categories.ComparableArrow;
 import org.upb.fmde.de.categories.diagrams.Diagram;
 
 public class CounterExampleChecker {
-	public static boolean isCounterExampleForMono(Diagram<FinSet, TotalFunction> d){
-		// TODO (4) Implement check for mono counter example
+	public static <O, A extends ComparableArrow<A>> boolean isCounterExampleForMono(Diagram<O, A> d) {
+		// TODO (4) Implement generic check for mono counter example
 		// Given: X -g-> Y -f-> Z
-		//        X -h-> Y -f-> Z
+		// X -h-> Y -f-> Z
 		// Check: (g;f = h;f) ^ (g != h)
-		TotalFunction g = d.getArrow("g");
-		TotalFunction f = d.getArrow("f");
-		TotalFunction h = d.getArrow("h");
+		var g = d.getArrow("g");
+		var f = d.getArrow("f");
+		var h = d.getArrow("h");
+
+		var cat = d.getCat();
+
 		throw new UnsupportedOperationException("This method has not been implemented yet!");
 	}
-	
-	public static boolean isCounterExampleForEpi(Diagram<FinSet, TotalFunction> d){
-		// TODO (5) Implement check for epi counter example
+
+	public static <O, A extends ComparableArrow<A>> boolean isCounterExampleForEpi(Diagram<O, A> d) {
+		// TODO (5) Implement generic check for epi counter example
 		// Given: Z -f-> Y -g-> X
-		//        Z -f-> Y -h-> X
+		// Z -f-> Y -h-> X
 		// Check: (f;g = f;h) ^ (g != h)
-		TotalFunction g = d.getArrow("g");
-		TotalFunction f = d.getArrow("f");
-		TotalFunction h = d.getArrow("h");
+		var g = d.getArrow("g");
+		var f = d.getArrow("f");
+		var h = d.getArrow("h");
+
+		var cat = d.getCat();
+
 		throw new UnsupportedOperationException("This method has not been implemented yet!");
 	}
 
